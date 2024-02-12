447ada354fa4b521a0cb5975ed0e4481f138f3c6
Bishwo Bista <bishwo5bsita@gmail.com>
2024-02-12 15:46:38 +0545

    update .env.example && add service provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-//        \Spatie\Multitenancy\MultitenancyServiceProvider::class,
+        \Spatie\Multitenancy\MultitenancyServiceProvider::class,
     ])->toArray(),
 
     /*
